8b234e554f4af1e764221f29af1e66892c60ab22
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2021-05-03 17:34:44 -0300

    build: 1.0.0-next.1 (#4)
    * feat(design-tokens): tokens globais e das marcas (#1)
    
    Tokens para o piloto
    
    * fix(size): correção dos valores (#2)
    
    * feat(design-tokens): tokens globais e das marcas
    
    Tokens para o piloto
    
    * feat(design-tokens): correção dos tokens do fluig
    
    * fix: correção dos tokens
    
    * feat: atualiza nome do pacote e tag (#3)
    
    Atualiza nome do pacote no NPM e coloca tag DEV como padrão
    
    * build: 1.0.0-next.1

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const { series, src, dest } = require('gulp');
+
+const tap = require('gulp-tap');
+const del = require('del');
+const { generateTokens } = require('./build');
+
+function clean (cb) {
+  del.sync('./dist');
+  cb();
+}
+
+function build(cb) {
+  generateTokens();
+  cb();
+}
+
+function copyPackageJson(cb) {
+  const packages = [
+    { name: '@animaliads/android-tokens', dest: './dist/global/android', desc: 'Tokens globais do Animalia DS para Android' },
+    { name: '@animaliads/css-tokens', dest: './dist/global/css', desc: 'Tokens globais do Animalia DS em CSS' },
+    { name: '@animaliads/ios-tokens', dest: './dist/global/ios', desc: 'Tokens globais do Animalia DS para IOS' },
+    { name: '@animaliads/ios-swift-tokens', dest: './dist/global/ios-swift', desc: 'Tokens globais do Animalia DS para IOS Swift' },
+    { name: '@animaliads/scss-tokens', dest: './dist/global/scss', desc: 'Tokens globais do Animalia DS em SCSS' },
+    { name: '@animaliads/fluig-tokens', dest: './dist/brands/fluig', desc: 'Tokens de brand do Fluig' },
+    { name: '@animaliads/po-ui-tokens', dest: './dist/brands/po-ui', desc: 'Tokens de brand do PO UI' },
+  ];
+
+  packages.forEach(package => {
+    src('package.json')
+    .pipe(
+      tap(file => {
+        const contents = JSON.parse(file.contents.toString());
+
+        delete contents.devDependencies;
+        delete contents.scripts;
+
+        contents.name = package.name;
+        contents.description = package.desc;
+
+        file.contents = Buffer.from(JSON.stringify(contents, null, 2), 'utf-8');
+      })
+    )
+    .pipe(dest(package.dest));
+  });
+  
+  cb();
+
+}
+
+exports.build = build;
+exports.default = series(clean, build, copyPackageJson);
